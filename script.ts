822d71f53a796c879a68232110d5e25d81d04872
isakmedia <isak.westerberg@gmail.com>
2023-04-16 16:47:11 +0200

    trying to add fold down

--- script.ts
+++ script.ts
@@ -1,2 +1,19 @@
+
 const openEl = document.querySelector("#menuOpen")
 const closeEl = document.querySelector("#menuClose")
+
+// OPEN NAVBAR
+openEl!.addEventListener('click', () => {
+
+    document.querySelector("#menuOpen")?.classList.add("hide");
+
+    document.querySelector("#menuClose")?.classList.remove("hide");
+});
+
+// CLOSE NAVBAR
+closeEl!.addEventListener('click', () => {
+
+    document.querySelector("#menuClose")?.classList.add("hide");
+
+    document.querySelector("#menuOpen")?.classList.remove("hide");
+});
