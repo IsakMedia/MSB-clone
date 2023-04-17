1cd4495132e59ef340b12dd1e94cb7089a24ac5a
isakmedia <isak.westerberg@gmail.com>
2023-04-17 14:39:29 +0200

    added a navbar (ish, wrong position), and started with hero pic

--- script.ts
+++ script.ts
@@ -1,6 +1,7 @@
 
 const openEl = document.querySelector("#menuOpen")
 const closeEl = document.querySelector("#menuClose")
+const menuOutEl = document.querySelector("#menuOut")
 
 // OPEN NAVBAR
 openEl!.addEventListener('click', () => {
@@ -8,6 +9,8 @@ openEl!.addEventListener('click', () => {
     document.querySelector("#menuOpen")?.classList.add("hide");
 
     document.querySelector("#menuClose")?.classList.remove("hide");
+
+    menuOutEl?.classList.remove("hide");
 });
 
 // CLOSE NAVBAR
@@ -16,4 +19,6 @@ closeEl!.addEventListener('click', () => {
     document.querySelector("#menuClose")?.classList.add("hide");
 
     document.querySelector("#menuOpen")?.classList.remove("hide");
+
+    menuOutEl?.classList.add("hide");
 });
