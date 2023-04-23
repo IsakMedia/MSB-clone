5b39ae17a1019bff0a646e2fb214ad1e8b7ca4ea
isakmedia <isak.westerberg@gmail.com>
2023-04-23 20:02:44 +0200

    adding memes

--- src/script.ts
+++ src/script.ts
@@ -2,6 +2,9 @@
 const openEl = document.querySelector("#menuOpen")
 const closeEl = document.querySelector("#menuClose")
 const menuOutEl = document.querySelector("#menuOut")
+const memeBtnEl = document.querySelector("#memeBtn")
+const memeBoxEl = document.querySelector('#memeBox')
+const dismissBtnEl = document.querySelector('#dismissBtn')
 
 // OPEN NAVBAR
 openEl!.addEventListener('click', () => {
@@ -22,3 +25,30 @@ closeEl!.addEventListener('click', () => {
 
     menuOutEl?.classList.add("hide");
 });
+
+
+
+memeBtnEl!.addEventListener('click', () => {
+
+
+        memeBoxEl!.classList.remove('hide')
+        memeBoxEl!.classList.add('show')
+
+    })
+    
+
+
+dismissBtnEl!.addEventListener('click', () => {
+    memeBoxEl!.classList.add('hide');
+    memeBoxEl!.classList.add('show')
+
+})
+
+
+window.addEventListener('DOMContentLoaded', () => {
+    setTimeout(() => {
+      memeBoxEl!.classList.add('show');
+    }, 1000);
+  });
+
+
